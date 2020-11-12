d626df5fc718ac18d5ea0f7e97115edf4361135e
Leonid Velichko <vlsrnd@gmail.com>
2020-11-12 17:33:47 +0300

    add 3 button, code edit button, code remove button

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const container = document.querySelector('.container');
 const inputText = document.querySelector('.input-text');
 
 
-let moveTempElement;
+let moveTempElement, moveBtnElement;
 //firstElement move to the center of screen
 document.addEventListener('DOMContentLoaded', () => {
   moveElement(firstElement, {clientX: document.documentElement.clientWidth / 2,
@@ -12,14 +12,23 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
 document.addEventListener('mousedown', (event) => {
-  if (event.target.dataset.name !== 'element') return;
+  if (event.target.dataset.name === 'element') {
+    //bind function moveElement
+    const tempElem = moveElement(createElement(event.target), event);
+    const bindMoveElement = elem => event => moveElement(elem, event);
+    moveTempElement = bindMoveElement(tempElem);
 
-  //bind function moveElement
-  const tempElem = moveElement(createElement(event.target), event);
-  const bindMoveElement = elem => event => moveElement(elem, event)
-  moveTempElement = bindMoveElement(tempElem);
+    document.addEventListener('mousemove', moveTempElement);
+  } else if (event.target.dataset.func === 'move') {
+    //перемещение
+    //так не получилось
+    // const bindMoveBtnElement = elem => event => moveElement(elem, event);
+    // moveBtnElement = bindMoveBtnElement(event.parentNode);
 
-  document.addEventListener('mousemove', moveTempElement);
+    // document.addEventListener('mousemove', moveBtnElement);
+    // сделать перемещение за кнопку
+    console.log(event.target);
+  }
 })
 //Заканчиваем перемещение
 document.addEventListener('mouseup', (event) => {
@@ -33,7 +42,10 @@ function createElement(obj) {
   newElement.textContent = 'New';
   newElement.dataset.id = ++obj.dataset.id;
   newElement.dataset.name = 'element';
-  newElement.insertAdjacentHTML('beforeend', '<div class="elements__edit" data-func="edit">&#9998;</div>')
+  newElement.insertAdjacentHTML('beforeend', '<div class="elements__btn elements__edit" data-func="edit">&#9998;</div>'
+                                + '<input class="input hide" type="text"></input>'
+                                + '<div class="elements__btn elements__remove" data-func="remove">&#10008;</div>'
+                                + '<div class="elements__btn elements__move" data-func="move">&#9995;</div>');
   container.append(newElement);
   console.log(newElement)
   return newElement;
@@ -45,26 +57,22 @@ function moveElement(elem, event) {
   return elem;
 }
 
-//edit listeners мутный кусок
 document.addEventListener('click', event => {
   if (event.target.dataset.func === 'edit') {
-    const parent = event.target.parentNode;
-    parent.innerHTML = '<input type="text"></input>'
-    const input = parent.querySelector('input');
-    input.classList.add('input');
-  }
+    event.target.previousSibling.textContent = '';
+    event.target.nextElementSibling.classList.remove('hide');
+    event.target.nextElementSibling.focus();
 
-  function changeText(event) {
-    if (event.keyCode === 13){
-      const parent = event.target.parentNode;
-      text = event.target.value;
-      // console.log(text);
-      parent.innerHTML = text;
-      parent.insertAdjacentHTML('beforeend', '<div class="elements__edit" data-func="edit">&#9998;</div>')
-
+    function forListenerKeydown(event) {
+      if (event.keyCode === 13) {
+        event.target.parentNode.firstChild.textContent = event.target.value;
+        event.target.classList.add('hide');
+        event.target.removeEventListener('keydown', forListenerKeydown);
+      }
     }
+    event.target.nextElementSibling.addEventListener('keydown', forListenerKeydown)
+  } else if (event.target.dataset.func === 'remove') {
+    if (document.querySelectorAll('.elements').length <= 1) return;
+    event.target.parentNode.remove();
   }
-
-  parent.addEventListener('keydown', changeText);
-
 })
