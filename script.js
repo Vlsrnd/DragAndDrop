23abeb0b7ced79690685a5afc51a22341d5037f2
Leonid Velichko <vlsrnd@gmail.com>
2020-11-18 15:42:50 +0300

    add openTrash function

--- script.js
+++ script.js
@@ -360,3 +360,52 @@ function getOutputStructure(col, arr) {
   result.push('</li></ul>');
   return result.flat(Infinity).filter(Boolean);
 }
+
+// open trash list
+function openTrash() {
+  if (!trashCollection.size) {
+    trash.style.transform = 'scale(1.2)';
+    setTimeout(() => {
+      trash.style.transform = 'scale(1)';
+    }, 100)
+  } else {
+
+    const mainTrashElement = document.createElement('div');
+    document.body.append(mainTrashElement);
+    mainTrashElement.style.top = trash.offsetTop - 50 + 'px';
+    mainTrashElement.style.left = trash.offsetLeft - 20 + 'px';
+    mainTrashElement.classList.add('trash__list');
+
+    const trashList = Array.from(trashCollection.entries())
+                            .map(elem => [elem[0], elem[1].value]);
+    trashList.forEach(elem => {
+      const deletedElem = document.createElement('div');
+      deletedElem.textContent = elem[1];
+      mainTrashElement.append(deletedElem);
+
+      //add listener
+      mainTrashElement.addEventListener('click', (e) => {
+        console.log(e.target);
+        window.requestAnimationFrame(() => {
+          mainTrashElement.style.height = '0px';
+        });
+          mainTrashElement.addEventListener('transitionend', () => {
+            mainTrashElement.remove();
+          })
+      });
+    })
+    window.requestAnimationFrame(() => {
+      mainTrashElement.style.width = trash.clientWidth + 40 + 'px';
+      mainTrashElement.style.height = trash.clientHeight + 50 + 'px';
+    })
+  }
+}
+
+trash.addEventListener('click', openTrash);
+
+
+
+//Создавать новые элементы отдельно
+//При создании вызывать функцию edit
+//Добвалять связи между двумя элементами
+//Добвалять наброски
