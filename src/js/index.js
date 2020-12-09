81f7b66478d106225936828289219cff8a3b457e
Leonid Velichko <vlsrnd@gmail.com>
2020-12-09 15:40:13 +0300

    resize functions are optimized

--- src/js/index.js
+++ src/js/index.js
@@ -52,7 +52,6 @@ document.addEventListener('mousedown', event => {
   }
 })
 
-//resize block
 +function () {
   window.addEventListener('resize', resizeThrottler, false);
   let resizeTimeout;
@@ -60,20 +59,14 @@ document.addEventListener('mousedown', event => {
     if (!resizeTimeout) {
       resizeTimeout = setTimeout(() => {
         resizeTimeout = null;
+        // resize handler start
         resizeCanvas(canvasBG, drawArea);
         repositionElements(elementsCollection, lastDrawAreaSize, drawArea);
+        // end
       }, 30)
     }
   }
 }();
-// window.requestAnimationFrame(resizeWindow);
-
-// window.addEventListener('resize', () => {
-//   resizeCanvas(canvasBG, drawArea);
-//   repositionElements(elementsCollection, lastDrawAreaSize,
-//                      drawArea, moveElement);
-// });
-
 
 
 
@@ -85,56 +78,7 @@ document.addEventListener('mousedown', event => {
 //   }
 // })
 
-// class Element {
-//   constructor(parent, coordX, coordY, destination, collection){
-//     this.element = null;
-//     this.value = 'new';
-//     this.parent = parent;
-//     this.children = [];
-//     this.clientX = coordX;
-//     this.clientY = coordY;
-//     this.destination = destination;
-//     this.collection = collection;
-//     this.moveLimits = {
-//       top: 0,
-//       bottom: this.destination.clientHeight,
-//       left: 0,
-//       right: this.destination.clientWidth,
-//     };
-//     this.htmlStructure = `<div class="element__text" data-func="element">${this.value}</div>`
-//                         +'<input class="element__input hide" type="text"></input>'
-//                         +'<div class="element__btn element__btn-remove" data-func="remove" title="remove">&#10008;</div>'
-//                         +'<div class="element__btn element__btn-create" data-func="create" title="create">+</div>'
-//                         +'<div class="element__btn element__btn-edit" data-func="edit" title="edit">&#9998;</div>'
-//                         +'<div class="element__btn element__btn-info" data-func="info" title="info">i</div>';
-//   }
-//   create() {
-//     const newElement = document.createElement('div');
-//     newElement.classList.add('element');
-//     newElement.dataset.func = 'element';
-//     newElement.insertAdjacentHTML('beforeend', this.htmlStructure);
-//     this.element = newElement;
-//     //add to collection
-//     this.collection.set(this.element, this);
-//     //append to
-//     this.destination.append(this.element);
-//     this.addChildrenPropForParent(this.collection, this.parent)
-//     this.move(this);
-//     return this; 
-//   }
 
-//   move({clientX, clientY} = this) {
-//     const x = clientX - this.destination.offsetLeft - this.element.clientWidth / 2;
-//     const y = clientY - this.destination.offsetTop - this.element.clientHeight / 2;
-//     this.element.style.left = `${Math.max(this.moveLimits.left, Math.min(this.moveLimits.right - this.element.clientWidth, x))}px`;
-//     this.element.style.top = `${Math.max(this.moveLimits.top, Math.min(this.moveLimits.bottom - this.element.clientHeight, y))}px`;
-//     this.clientX = this.element.offsetLeft + this.element.clientWidth / 2;
-//     this.clientY = this.element.offsetTop + this.element.clientHeight / 2;
-//   }
-
-//   addChildrenPropForParent(collection, parent){
-//     if (this.parent) collection.get(parent).children.push(this.element);
-//   }
 //   edit(){
 //     const input = this.element.querySelector('.element__input');
 //     const text = this.element.querySelector('.element__text');
